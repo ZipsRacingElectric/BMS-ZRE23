593f7ee4bc095d4d3e5bcd9307acbf4004bef00e
FalcioneE <31711486+FalcioneE@users.noreply.github.com>
2022-02-10 23:10:01 -0500

    add check for faults prototype

--- BMS22.X/fault_handler.h
+++ BMS22.X/fault_handler.h
@@ -18,6 +18,7 @@ void fault_handler_initialize(void);
 void increment_cell_voltage_fault(uint8_t cell_id);
 void reset_cell_voltage_fault(uint8_t cell_id);
 uint8_t get_fault_codes(void);
+void check_for_fault(void);
 
 #ifdef	__cplusplus
 }
